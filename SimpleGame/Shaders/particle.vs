6d20bebcfb868ae6ad76297cfd820eb9e9fc859a
boowerk <back20044@gmail.com>
2025-09-23 16:12:14 +0900

    Add vel, startTime, shader hot reloading

--- SimpleGame/Shaders/particle.vs
+++ SimpleGame/Shaders/particle.vs
@@ -3,23 +3,36 @@
 in vec3 a_Position;
 in float a_Radius;
 in vec4 a_Color;
+in float a_STime;
+in vec3 a_Vel;
 
 out vec4 v_Color;
 
 uniform float u_Time;
 
-const float c_PI = 3.141592;
+const float	c_PI	= 3.141592;
+const vec2	c_G		= vec2(0, -9.8);
 
 void main()
 {
-	float value = fract(u_Time) * 2 - 1;		// -1 ~ 1
-	float rad = (value + 1) * c_PI;				// 0 - 2PI
-	float x = a_Radius * cos(rad);
-	float y = a_Radius * sin(rad);
+	float newTime	= u_Time - a_STime;
 
-	vec4 newPosition = vec4(a_Position, 1.0f);
-	newPosition.xy = newPosition.xy + vec2(x, y);
-	gl_Position = newPosition;
+	vec4 newPosition	= vec4(a_Position, 1.0f);
+	
+	if (newTime > 0)
+	{
+		float t			= fract(newTime / 2.0) * 2.0;
+		float tt		= t * t;
+		float x			= 0;
+		float y			= 0.5 * c_G.y * tt;
+		newPosition.xy	+= vec2(x, y);
+	}
+	else 
+	{
+		newPosition.xy	+= vec2(-100000, 0);
+	}
+
+	gl_Position			= newPosition;
 
 	v_Color = a_Color;
 }
--- SimpleGame/SimpleGame.cpp
+++ SimpleGame/SimpleGame.cpp
@@ -2,7 +2,7 @@
 Copyright 2022 Lee Taek Hee (Tech University of Korea)
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the What The Hell License. Do it plz.
+it under the terms of the What The Hell License. Do it plz. 
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY.
@@ -17,8 +17,16 @@ but WITHOUT ANY WARRANTY.
 
 Renderer *g_Renderer = NULL;
 
+bool g_bNeedReloadShaderProgram = false;
+
 void RenderScene(void)
 {
+	if (g_bNeedReloadShaderProgram)
+	{
+		g_Renderer->ReloadAllShaderProgram();
+		g_bNeedReloadShaderProgram = false;
+	}
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
 
@@ -37,17 +45,22 @@ void Idle(void)
 
 void MouseInput(int button, int state, int x, int y)
 {
-	RenderScene();
 }
 
 void KeyInput(unsigned char key, int x, int y)
 {
-	RenderScene();
+	switch (key)
+	{
+	case '1':
+		g_bNeedReloadShaderProgram = true;
+		break;
+	default:
+		break;
+	}
 }
 
 void SpecialKeyInput(int key, int x, int y)
 {
-	RenderScene();
 }
 
 int main(int argc, char **argv)
