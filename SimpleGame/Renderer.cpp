f38511f60a2010388f650037f405d03b4d1ddf10
boowerk <back20044@gmail.com>
2025-09-30 16:16:24 +0900

    Add mass and period to particle attributes

--- SimpleGame/Renderer.cpp
+++ SimpleGame/Renderer.cpp
@@ -23,7 +23,7 @@ void Renderer::Initialize(int windowSizeX, int windowSizeY)
 	//Create VBOs
 	CreateVertexBufferObjects();
 
-	GenerateParticles(10000);
+	GenerateParticles(50000);
 
 	if (m_SolidRectShader > 0 && m_VBORect > 0)
 	{
@@ -284,17 +284,19 @@ void Renderer::DrawTest()
 		"u_Time");
 	glUniform1f(uTimeLoc, m_time);
 
+
+
 	int aPosLoc = glGetAttribLocation(m_TestShader, "a_Position");
-	int aRadiusLoc = glGetAttribLocation(m_TestShader, "a_Radius");
+	int aValueLoc = glGetAttribLocation(m_TestShader, "a_Value");
 	int aColorLoc = glGetAttribLocation(m_TestShader, "a_Color");
 
 	glEnableVertexAttribArray(aPosLoc);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBOtestPos);
 	glVertexAttribPointer(aPosLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 4, 0);
 
-	glEnableVertexAttribArray(aRadiusLoc);
+	glEnableVertexAttribArray(aValueLoc);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBOtestPos);
-	glVertexAttribPointer(aRadiusLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (GLvoid*)(sizeof(float)*3));
+	glVertexAttribPointer(aValueLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (GLvoid*)(sizeof(float)*3));
 
 	glEnableVertexAttribArray(aColorLoc);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBOtestColor);
@@ -304,7 +306,7 @@ void Renderer::DrawTest()
 	glDrawArrays(GL_TRIANGLES, 0, 12);
 
 	glDisableVertexAttribArray(aPosLoc);
-	glDisableVertexAttribArray(aRadiusLoc);
+	glDisableVertexAttribArray(aValueLoc);
 	glDisableVertexAttribArray(aColorLoc);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -325,45 +327,46 @@ void Renderer::DrawParticle()
 		"u_Time");
 	glUniform1f(uTimeLoc, m_time);
 
+	int uForceLoc = glGetUniformLocation(shader,
+		"u_Force");
+	glUniform3f(uForceLoc, std::sin(m_time), 0, 0);
+
 	int aPosLoc = glGetAttribLocation(shader, "a_Position");
-	int aRadiusLoc = glGetAttribLocation(shader, "a_Radius");
+	int aValueLoc = glGetAttribLocation(shader, "a_Value");
 	int aColorLoc = glGetAttribLocation(shader, "a_Color");
 	int aSTimeLoc = glGetAttribLocation(shader, "a_STime");
 	int aVelLoc = glGetAttribLocation(shader, "a_Vel");
 	int aLifeTimeLoc = glGetAttribLocation(shader, "a_LifeTime");
+	int aMassLoc = glGetAttribLocation(shader, "a_Mass");
+	int aPeriodLoc = glGetAttribLocation(shader, "a_Period");
 
-	int stride = 13;
+	int stride = 15;
 
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
 	glEnableVertexAttribArray(aPosLoc);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
-	glVertexAttribPointer(aPosLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, 0);
-
-	glEnableVertexAttribArray(aRadiusLoc);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
-	glVertexAttribPointer(aRadiusLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 3));
-
+	glEnableVertexAttribArray(aValueLoc);
 	glEnableVertexAttribArray(aColorLoc);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
-	glVertexAttribPointer(aColorLoc, 4, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 4));
-
 	glEnableVertexAttribArray(aSTimeLoc);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
-	glVertexAttribPointer(aSTimeLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 8));
-
 	glEnableVertexAttribArray(aVelLoc);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
-	glVertexAttribPointer(aVelLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 9));
-
 	glEnableVertexAttribArray(aLifeTimeLoc);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOParticle);
+	glEnableVertexAttribArray(aMassLoc);
+	glEnableVertexAttribArray(aPeriodLoc);
+	
+	glVertexAttribPointer(aPosLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, 0);
+	glVertexAttribPointer(aValueLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 3));
+	glVertexAttribPointer(aColorLoc, 4, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 4));
+	glVertexAttribPointer(aSTimeLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 8));
+	glVertexAttribPointer(aVelLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 9));
 	glVertexAttribPointer(aLifeTimeLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 12));
+	glVertexAttribPointer(aMassLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 13));
+	glVertexAttribPointer(aPeriodLoc, 1, GL_FLOAT, GL_FALSE, sizeof(float) * stride, (GLvoid*)(sizeof(float) * 14));
 
 	
 
 	glDrawArrays(GL_TRIANGLES, 0, m_VBOParticleVertexCount);	// m_VBOParticleVertexCount
 
 	glDisableVertexAttribArray(aPosLoc);
-	glDisableVertexAttribArray(aRadiusLoc);
+	glDisableVertexAttribArray(aValueLoc);
 	glDisableVertexAttribArray(aColorLoc);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
@@ -379,7 +382,7 @@ void Renderer::GetGLPosition(float x, float y, float *newX, float *newY)
 
 void Renderer::GenerateParticles(int numParticle)
 {
-	int floatCountPervertex = 3 + 1 + 4 + 1 + 3 + 1; // x, y, z, value, r, g, b, a, sTime, vx, vy, vz, lifeTime
+	int floatCountPervertex = 3 + 1 + 4 + 1 + 3 + 1 + 1 + 1; // x, y, z, value, r, g, b, a, sTime, vx, vy, vz, lifeTime, mass, period
 	int verticesCountPerParticle = 6;
 	int floatCountPerParticle = floatCountPervertex * verticesCountPerParticle;
 	int totalVerticesCount = numParticle * verticesCountPerParticle;
@@ -400,13 +403,15 @@ void Renderer::GenerateParticles(int numParticle)
 		b =  (float)rand() / (float)RAND_MAX;
 		a =  (float)rand() / (float)RAND_MAX;
 		float size;
-		size = ((float)rand() / (float)RAND_MAX) * 0.007f;
+		size = ((float)rand() / (float)RAND_MAX) * 0.01f;
 		float sTime = ((float)rand() / (float)RAND_MAX) * 2.f;
 		float vx, vy, vz;
-		vx = ((float)rand() / (float)RAND_MAX * 2.f - 1.f) * 1.0f;
-		vy = (((float)rand() / (float)RAND_MAX * 2.f - 1.f) + 1.5f) * 0.8f;
+		vx = 0; ((float)rand() / (float)RAND_MAX * 2.f - 1.f) * 1.0f;
+		vy = 0; (((float)rand() / (float)RAND_MAX * 2.f - 1.f) + 1.5f) * 0.8f;
 		vz = 0.f;
-		lifeTime = (float)rand() / (float)RAND_MAX * 2.f;
+		lifeTime = (float)rand() / (float)RAND_MAX * 1.f;
+		float mass = (float)rand() / (float)RAND_MAX + 1.f;
+		float period = (float)rand() / (float)RAND_MAX + 1.f;
 
 		int index = i * floatCountPervertex * verticesCountPerParticle;	
 
@@ -423,6 +428,9 @@ void Renderer::GenerateParticles(int numParticle)
 		vertices[index++] = vy;
 		vertices[index++] = vz;
 		vertices[index++] = lifeTime;
+		vertices[index++] = mass;
+		vertices[index++] = period;
+		
 
 		vertices[index++] = x + size;		// v2
 		vertices[index++] = y - size;
@@ -437,6 +445,8 @@ void Renderer::GenerateParticles(int numParticle)
 		vertices[index++] = vy;
 		vertices[index++] = vz;
 		vertices[index++] = lifeTime;
+		vertices[index++] = mass;
+		vertices[index++] = period;
 
 		vertices[index++] = x + size;		// v3
 		vertices[index++] = y + size;
@@ -451,6 +461,8 @@ void Renderer::GenerateParticles(int numParticle)
 		vertices[index++] = vy;
 		vertices[index++] = vz;
 		vertices[index++] = lifeTime;
+		vertices[index++] = mass;
+		vertices[index++] = period;
 
 		vertices[index++] = x - size;		// v4
 		vertices[index++] = y + size;
@@ -465,6 +477,8 @@ void Renderer::GenerateParticles(int numParticle)
 		vertices[index++] = vy;
 		vertices[index++] = vz;
 		vertices[index++] = lifeTime;
+		vertices[index++] = mass;
+		vertices[index++] = period;
 
 		vertices[index++] = x - size;		// v5
 		vertices[index++] = y - size;
@@ -479,6 +493,8 @@ void Renderer::GenerateParticles(int numParticle)
 		vertices[index++] = vy;
 		vertices[index++] = vz;
 		vertices[index++] = lifeTime;
+		vertices[index++] = mass;
+		vertices[index++] = period;
 
 		vertices[index++] = x + size;		// v6
 		vertices[index++] = y + size;
@@ -493,6 +509,8 @@ void Renderer::GenerateParticles(int numParticle)
 		vertices[index++] = vy;
 		vertices[index++] = vz;
 		vertices[index++] = lifeTime;
+		vertices[index++] = mass;
+		vertices[index++] = period;
 	}
 
 	glGenBuffers(1, &m_VBOParticle);
--- SimpleGame/Shaders/particle.vs
+++ SimpleGame/Shaders/particle.vs
@@ -1,20 +1,23 @@
 #version 330
 
 in vec3 a_Position;
-in float a_Radius;
+in float a_Value;
 in vec4 a_Color;
 in float a_STime;
 in vec3 a_Vel;
 in float a_LifeTime;
+in float a_Mass;
+in float a_Period;
 
 out vec4 v_Color;
 
 uniform float u_Time;
+uniform vec3 u_Force;
 
-const float c_PI = 3.141592;
-const vec2 c_G = vec2(0, -9.8);
+const float c_PI		= 3.141592;
+const vec2 c_G			= vec2(0, -9.8);
 
-void main()
+void raining()
 {
 	float lifeTime = a_LifeTime;
 	float newAlpha = 1.0f;
@@ -25,8 +28,16 @@ void main()
 	{
 		float t = fract(newTime / lifeTime) * lifeTime;
 		float tt = t*t;
-		float x = a_Vel.x * t + 0.5 * c_G.x * 0.2 * tt;
-		float y = (a_Vel.y + 1.0f) * t + 0.5 * (c_G.y * 3.f) * 0.2 * tt - 0.5f;
+
+
+		float forceX = u_Force.x + c_G.x * a_Mass;
+		float forceY = u_Force.y + c_G.y * a_Mass;
+
+		float aX = forceX / a_Mass * 50;
+		float aY = forceY / a_Mass;
+
+		float x = a_Vel.x * t + 0.5 * aX * 0.2 * tt;
+		float y = a_Vel.y * t + 0.5 * (aY * 3.f) * 0.2 * tt;
 
 		newPosition.xy += vec2(x, y);
 		newAlpha = 1.0f - t/lifeTime;	// 1-0
@@ -38,4 +49,80 @@ void main()
 
 	gl_Position = newPosition;
 	v_Color = vec4(a_Color.rgb, newAlpha);
+}
+
+void sinParticle()
+{
+	vec4 centerC		= vec4(1,0,0,1);
+	vec4 borderC		= vec4(1,1,1,0);
+
+	float newTime		= u_Time - a_STime; 
+	float lifeTime		= a_LifeTime;
+	float newAlpha		= 1.0f;
+	vec4 newColor		= a_Color;
+	vec4 newPosition	= vec4(a_Position, 1.0f);
+
+	if(newTime > 0){
+		float period	= a_Period * 4;
+		float t			= fract(newTime/lifeTime) * lifeTime;
+		float tt		= t * t;
+		float x			= t * 2 - 1.f;
+		float y			= t * sin(t * 2 * c_PI * period) * ((a_Value -0.5) * 2.f);
+
+		y *= sin(fract(newTime/lifeTime) * c_PI);
+		
+		newPosition.xy += vec2(x,y);
+		newAlpha = 1.0f - t/lifeTime;
+
+		newColor = mix(centerC, borderC, abs(y * 8));
+	}
+	else{	
+		newPosition.xy = vec2(-1000000,0);
+	}
+		
+
+	gl_Position = newPosition;
+	v_Color = vec4(newColor.rgb, newAlpha);	// a_Color
+}
+
+void circleParticle()
+{
+	float newTime = u_Time - a_STime; 
+	float lifeTime = a_LifeTime;
+	float newAlpha = 1.0f;
+	vec4 newPosition = vec4(a_Position, 1.0f);
+
+	if(newTime > 0)
+	{
+		float t = fract(newTime/lifeTime) * lifeTime;
+		float tt = t * t;
+		float theta = a_Value * 2.0 * c_PI;  // 0 ~ 2¥ð
+
+		float x = 16.0 * pow(sin(theta), 3.0);
+		float y = 13.0 * cos(theta) - 5.0 * cos(2.0 * theta)
+        - 2.0 * cos(3.0 * theta) - cos(4.0 * theta);
+
+		x *= 0.05;
+		y *= 0.05;
+
+		float newX = x + 0.5 * c_G.x * tt;
+		float newY = y + 0.5 * c_G.y * tt;
+
+		newPosition.xy += vec2(newX,newY);
+
+		newAlpha = 1.0f - t/lifeTime;
+	}
+	else
+	{
+		newPosition.xy = vec2(-1000000,0);
+	}
+	gl_Position = newPosition;
+	v_Color = vec4(a_Color.rgb, newAlpha);	
+}
+
+void main()
+{
+	// raining();
+	// sinParticle();
+	circleParticle();
 }
